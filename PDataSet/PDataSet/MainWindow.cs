84e53b754dc72f1d8f38b0c3e8af2852614cb0e6
pedroblasco <pedroblascoibarra@gmail.com>
2013-01-11 13:16:45 +0100

    Añadiendo PDataSet con los últimos cambios.

--- PDataSet/PDataSet/MainWindow.cs
+++ PDataSet/PDataSet/MainWindow.cs
@@ -28,5 +28,19 @@ public partial class MainWindow: Gtk.Window
 		
 		dbDataAdapter.Fill(dataSet);
 		Console.WriteLine("Tables.Count=(0)", dataSet.Tables.Count);
+		foreach(DataTable dataTable in dataSet.Tables)
+			show (dataTable);
  	}
+	
+	/*private void show(DataTable dataTable){
+		foreach(DataColumn dataColumn in dataTable.Columns)
+			Console.WriteLine("Column.Name={0}", dataColumn.ColumnName);*/
+		
+		foreach(DataRow dataRow in dataTable.Rows){
+			foreach(DataColumn dataColumn in dataTable.Columns)
+				Console.Write("[{0}={1}]", dataColumn.ColumnName, dataRow[dataColumn]);
+		Console.WriteLine ();
+		}
+	}
+	
 }
