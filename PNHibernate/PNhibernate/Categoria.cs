c0b15bf024163bc155bdedf80be1088eda98390c
pedroblasco <pedroblascoibarra@gmail.com>
2013-02-08 12:35:39 +0100

    Actualizando con ICriteria.

--- PNHibernate/PNhibernate/Categoria.cs
+++ PNHibernate/PNhibernate/Categoria.cs
@@ -6,6 +6,11 @@ namespace Serpis.Ad
 	{
 		public virtual long Id {get; set;}
 		public virtual string Nombre {get; set;}	
+		
+	 	public override string ToString ()
+		{
+			return string.Format ("[Categoria: Id={0}, Nombre={1}]", Id, Nombre);
+		}
 	}
 }
 
--- PNHibernate/PNhibernate/MainWindow.cs
+++ PNHibernate/PNhibernate/MainWindow.cs
@@ -1,11 +1,10 @@
-using System;
 using Gtk;
-
 using NHibernate;
-
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
-
+using Serpis.Ad;
+using System;
+using System.Collections;
 using Serpis.Ad;
 
 public partial class MainWindow: Gtk.Window
@@ -31,12 +30,13 @@ public partial class MainWindow: Gtk.Window
 		
 		//using(ISession session = sessionFactory.OpenSession()){
 		ISession session = sessionFactory.OpenSession();
-		ICriteria criteria = session.CreateCriteria(typeof(Articulo));
+		ICriteria criteria = session.CreateCriteria (typeof(Articulo));
+		criteria.SetFetchMode("Categoria", FetchMode.Join);
 		IList list = criteria.List();
-		foreach(Articulo articulo in list)
-			Console.WriteLine("Articulo  Id={0} Nombre={1} Precio={2}",
-			                  articulo.Id, articulo.Nombre, articulo.Precio);		
-			session.Close();
+		foreach (Articulo articulo in list)
+			Console.WriteLine("Articulo Id={0} Nombre={1} Precio={2} Categoria={3}", 
+			                  articulo.Id, articulo.Nombre, articulo.Precio, articulo.Categoria);
+		session.Close ();		
 			
 		sessionFactory.Close();
 	}
@@ -44,12 +44,13 @@ public partial class MainWindow: Gtk.Window
 	private void loadArticulo(ISessionFactory sessionFactory) {
 		using (ISession session = sessionFactory.OpenSession()) {
 			Articulo articulo = (Articulo)session.Load (typeof(Articulo), 2L);
-			Console.Write ("Articulo Id={0} Nombre={1} Precio={2}", 
+			Console.Write ("Articulo Id={0} Nombre={1} Precio={2} ", 
 			                   articulo.Id, articulo.Nombre, articulo.Precio);
-			if(articulo.Categoria == null)
-				Console.WriteLine("Categoria=null");
+			if (articulo.Categoria == null)
+				Console.WriteLine ("Categoria=null");
 			else
-				Console.WriteLine ("Categoria.Id{0}", articulo.Categoria.Id);
+				Console.WriteLine("Categoria.Id={0}", articulo.Categoria.Id);
+				//Console.WriteLine("Categoria.Nombre={0}", articulo.Categoria.Nombre);
 		}
 	}
 		
